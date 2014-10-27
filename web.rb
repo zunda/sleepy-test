77cae2905b30bd87f540550d68fa7730c4e7b8d9
zunda <zundan@gmail.com>
2014-10-26 18:08:31 -1000

    retab

--- web.rb
+++ web.rb
@@ -1,10 +1,11 @@
+# vim: ts=3
 require 'sinatra'
 
 get '/' do
-  # GET /?spinup=WAIT_SEC
-  spinup =  (params[:spinup] || 0).to_i
-  $stdout.puts "Sleeping for #{spinup} sec for spinup"
-  $stdout.flush
-  sleep spinup
-  "Hello, world after #{spinup} seconds"
+	# GET /?spinup=WAIT_SEC
+	spinup =	(params[:spinup] || 0).to_i
+	$stdout.puts "Sleeping for #{spinup} sec for spinup"
+	$stdout.flush
+	sleep spinup
+	"Hello, world after #{spinup} seconds"
 end
