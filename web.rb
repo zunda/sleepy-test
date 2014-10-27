b88e4bfc9bcf8c0b0bcf4c9caeec8c4a1d9de8ed
zunda <zundan@gmail.com>
2014-10-26 18:23:29 -1000

    Stream

--- web.rb
+++ web.rb
@@ -1,15 +1,25 @@
-# vim: ts=3
+# vim: ts=3 sw=3:
 require 'sinatra'
 
 get '/' do
+	content_type 'text/plain'
 	# GET /?spinup=WAIT_SEC;interval=INTERVAL_SEC;repeat=REPEAT_NUMBER
 	par = Hash.new
-	%i(spinup inrerval repat).each do |k|
+	%i(spinup interval repeat).each do |k|
 		par[k] = (params[k] || 0).to_i
 	end
-	spinup =	(par[:spinup] || 0).to_i
-	$stdout.puts "Sleeping for #{spinup} sec for spinup"
-	$stdout.flush
-	sleep spinup
-	"Hello, world after #{spinup} seconds"
+	stream do |out|
+		$stdout.puts "Sleeping for #{par[:spinup]} sec for spinup"
+		$stdout.flush
+		sleep par[:spinup]
+		out << "Hello, world after #{par[:spinup]} seconds\n"
+		par[:repeat].times do |i|
+			msg = "Slept for #{par[:interval]} sec (#{i+1}/#{par[:repeat]})\n"
+			sleep par[:interval]
+			$stdout.puts msg
+			$stdout.flush
+			out << msg
+		end
+		out << "Good bye for now\n"
+	end
 end
