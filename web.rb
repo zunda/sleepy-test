b22504db1384f30a6ff2e992e924a5905f5d661d
zunda <zundan@gmail.com>
2014-10-26 18:10:23 -1000

    Use hash to convert parameters

--- web.rb
+++ web.rb
@@ -2,8 +2,12 @@
 require 'sinatra'
 
 get '/' do
-	# GET /?spinup=WAIT_SEC
-	spinup =	(params[:spinup] || 0).to_i
+	# GET /?spinup=WAIT_SEC;interval=INTERVAL_SEC;repeat=REPEAT_NUMBER
+	par = Hash.new
+	%i(spinup inrerval repat).each do |k|
+		par[k] = (params[k] || 0).to_i
+	end
+	spinup =	(par[:spinup] || 0).to_i
 	$stdout.puts "Sleeping for #{spinup} sec for spinup"
 	$stdout.flush
 	sleep spinup
